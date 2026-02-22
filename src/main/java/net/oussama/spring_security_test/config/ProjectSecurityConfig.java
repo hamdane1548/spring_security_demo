39410ec5b64f70c553cc56a7d5bc64abe11fdc6f
hamdane1548 <oussamahamdane80@gmail.com>
2026-02-22 01:12:47 +0000

    password encoder test implmentation

--- src/main/java/net/oussama/spring_security_test/config/ProjectSecurityConfig.java
+++ src/main/java/net/oussama/spring_security_test/config/ProjectSecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +20,20 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) ->{
                     requests.requestMatchers("/myaccount","/mybalance","/cards","/loans").authenticated();
                     requests.requestMatchers("/contact","/notice","/error ").permitAll();});
-        http.formLogin(fls -> fls.disable());
+        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+       UserDetails user =  User.withUsername("oussama").password("{noop}12345").authorities("read").build();
+       UserDetails user2 =  User.withUsername("admin").password("{bcrypt}$2a$12$pKl1XPMbQH/YHyPjF4ZY4.YTCQVbHbmPAU4fPRXxqIOiN.mQa2KPS").authorities("admin").build();
+       return new InMemoryUserDetailsManager(user,user2);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
